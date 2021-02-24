961ce54aaa50fc4b9555ecf1b0495249081d8c44
gsporto <gabriel_porto@outlook.com>
2021-02-23 21:13:52 -0300

    add routing and styled page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Grid,
+  Link,
+  Paper,
+  TextField,
+} from "@material-ui/core";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { useStyles } from "./styles";
+
+export function SignIn() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Logo />
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Entrar
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Esqueci minha senha
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+}
